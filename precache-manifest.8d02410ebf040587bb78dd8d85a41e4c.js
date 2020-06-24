af8a3c386d81f71361ffc4e4054fbeba2e29b2fb
shayan <shayan@firstsource.tech>
2020-06-24 13:52:03 +0430

    Updates

---
+++ precache-manifest.8d02410ebf040587bb78dd8d85a41e4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df957b47a06120894e1de7d00c9f7869",
+    "url": "/test-circleci/index.html"
+  },
+  {
+    "revision": "b8b9c196288a7d78783a",
+    "url": "/test-circleci/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9996cbc257538b888e1",
+    "url": "/test-circleci/static/js/2.0450cb7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-circleci/static/js/2.0450cb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b9c196288a7d78783a",
+    "url": "/test-circleci/static/js/main.1a449983.chunk.js"
+  },
+  {
+    "revision": "f69dd668348566afe697",
+    "url": "/test-circleci/static/js/runtime-main.b31a93ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-circleci/static/media/logo.5d5d9eef.svg"
+  }
+]);
